b468fffd410459d576915220c87bbd7ff031744c
Ryan Twohey <rjtwoh01@gmail.com>
2022-02-07 17:52:27 -0500

    removed unused imports and code

--- hw2/monte_carlo.py
+++ hw2/monte_carlo.py
@@ -1,10 +1,6 @@
-import math
-import random
 import time
 from rng import random_uniform_sample, lcg
-# import pandas as pd
 
-#shipA.speed = int(input('Input the speed for Ship A: '))
 interval = int(input('Input the interval to find pi: '))
 
 circlePoints = 0
@@ -13,9 +9,6 @@ valuesOfPi = []
 seed = time.time()
 
 for i in range(interval**2):
-    # randX = random.uniform(-1,1)
-    # randY = random.uniform(-1,1)
-    #30, [-1, 1]
     rus = random_uniform_sample(2, [-1, 1], seed)
     randX = rus[0]
     randY = rus[1]
@@ -35,4 +28,3 @@ for i in range(interval**2):
 
 print('Interval range:', interval, ", with total iterations:", interval**2)
 print('Final estimation of Pi=', pi)
-# print('total iterations:', len(valuesOfPi))
