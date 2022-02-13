2823c34b2620953baaaadcc5c1e69873822173e2
Ryan Twohey <rjtwoh01@gmail.com>
2022-02-13 18:45:48 -0500

    coupled tank problem completed (probably)

---
+++ hw3/coupled_tank.py
@@ -0,0 +1,61 @@
+from audioop import avg
+import math
+from re import L
+import struct
+import time
+import rng
+
+class Tank(object):
+    def __init__(self):
+        self.a = 0
+        self.fin = 0
+        self.fout = 0
+        self.height = 0
+        self.heightDiff = []
+        self.heightList = []
+        self.averageHeight = 0
+        self.maxHeight = 0
+        self.averageHeightDiff = 0
+        self.maxHeightDiff = 0
+
+    def getHeight(self):
+        heightDifference = (self.fin - self.fout) / self.a
+        self.heightDiff.append(heightDifference)
+        self.height = self.height + heightDifference
+        self.heightList.append(self.height)
+
+    def calculateStats(self):
+        self.maxHeight = max(self.heightList)
+        self.averageHeight = sum(self.heightList) / len(self.heightList)
+        self.maxHeightDiff = max(self.heightDiff)
+        self.averageHeightDiff = sum(self.heightDiff) / len(self.heightDiff)
+
+
+def main():
+    tank1 = Tank(); tank2 = Tank()
+    a = float(input('Enter area for tank 1: '))
+    fin = float(input('Enter flow in for tank 1: '))
+    fout = float(input('Enter flow out for tank 1: '))
+    h = float(input('Enter starting height for tank 1: '))
+    tank1.a = a; tank1.fin = fin; tank1.fout = fout; tank1.h = h
+    a = float(input('Enter area for tank 2: '))
+    fin = float(input('Enter flow in for tank 2: '))
+    fout = float(input('Enter flow out for tank 2: '))
+    h = float(input('Enter starting height for tank 2: '))
+    tank2.a = a; tank2.fin = fin; tank2.fout = fout; tank2.height = h
+
+    length = int(input('Enter the length of the simulation: '))
+    i = 0
+    
+    while i < length:
+        tank1.getHeight()
+        tank2.getHeight()
+        i += 1
+    
+    tank1.calculateStats(); tank2.calculateStats()
+
+    print('Tank 1: Average height:', tank1.averageHeight, "Max Height:", tank1.maxHeight, "Max Height Diff:", tank1.maxHeightDiff, "Avg Height Diff:", tank1.averageHeightDiff)
+    print('Tank 2: Average height:', tank2.averageHeight, "Max Height:", tank2.maxHeight, "Max Height Diff:", tank2.maxHeightDiff, "Avg Height Diff:", tank2.averageHeightDiff)
+
+if __name__ == "__main__":
+    main()
---
+++ hw3/rng.py
@@ -0,0 +1,44 @@
+import math
+import time
+
+def lcg(x, a, c, m):
+    while True:
+        x = (a * x + c) % m
+        yield x
+
+def random_uniform_sample(n, interval, seed = time.time()):
+    a, c, m = 1103515245, 12345, 2 ** 31
+    #seed = 1644260284.448245
+    bsdrand = lcg(seed, a, c, m)
+
+    lower, upper = interval[0], interval[1]
+    sample = []
+
+    for i in range(n):
+        observation = (upper - lower) * (next(bsdrand) / (2 ** 31 - 1)) + lower
+        sample.append(observation)
+
+    return sample
+
+
+def random_uniform_sample_integer(n, interval, seed = time.time()):
+    a, c, m = 1103515245, 12345, 2 ** 31
+    #seed = 1644260284.448245
+    bsdrand = lcg(seed, a, c, m)
+
+    lower, upper = interval[0], interval[1]
+    sample = []
+
+    for i in range(n):
+        observation = (upper - lower) * (next(bsdrand) / (2 ** 31 - 1)) + lower
+        sample.append(round(observation))
+        sample.append(observation)
+
+    return sample
+
+def main():
+    rus = random_uniform_sample(30, [-1, 1])
+    print(rus)
+
+if __name__ == "__main__":
+    main()
