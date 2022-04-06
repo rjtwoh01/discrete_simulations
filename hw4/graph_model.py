c88e6dfe44948557ae9d78a7ce4ff3c6db39c288
Ryan Twohey <rjtwoh01@gmail.com>
2022-04-06 19:50:33 -0400

    correctly iterate through all the paths for each simulation and extract the values for each path

--- hw4/graph_model.py
+++ hw4/graph_model.py
@@ -10,12 +10,17 @@ graph = {
     6: {7: (8, 12)}
 }
 
+traversalTimes = {
+    '1,2': (3,5),
+    '1, 5': (6)
+}
+
 
 def find_path(graph, start, end, path=[]):
     path = path + [start]
     if start == end:
         return path
-    if not graph.has_key(start):
+    if not start in graph:
         return None
     for node in graph[start]:
         if node not in path:
@@ -27,7 +32,7 @@ def find_all_paths(graph, start, end, path=[]):
     path = path + [start]
     if start == end:
         return [path]
-    if not graph.has_key(start):
+    if not start in graph:
         return []
     paths = []
     for node in graph[start]:
@@ -38,15 +43,51 @@ def find_all_paths(graph, start, end, path=[]):
     return paths
 
 
-def find_shortest_path(graph, start, end):
-    dist = {start: [start]}
-    q = deque(start)
-    while len(q):
-        at = q.popleft()
-        for next in graph[at]:
-            if next not in dist:
-                dist[next] = [dist[at], next]
-                q.append(next)
-    return dist.get(end)
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
-find_path(graph, '1', '7')
+print(find_all_paths(graph, 1, 7))
+traversalTime = []
+print(graph)
+
+count = 0
+simulationCount = int(input('Enter how many times to run the simulation: '))
+printProgressBar(0, simulationCount, prefix = 'Progress:', suffix = 'Complete', length = 50)
+while count < simulationCount: 
+    pathList = find_all_paths(graph, 1, 7)
+    for path in pathList:
+        # print(path)
+        parentNode = path[0]
+        # print(parentNode)
+        # nodeIndex = 0
+        for node in path:
+            if node != 1 & parentNode != 7:
+                # graphIndex = list(graph).index(parentNode)
+                graphNode = graph[parentNode]
+                # print('graphNode:',graphNode)
+                # print('parentNode:',parentNode)
+                # print('node:',node)
+                value = graphNode[node]
+                # print('value:',value)
+                parentNode = node
+            # print('node before update:', node)
+    count += 1
+    printProgressBar(count, simulationCount, prefix = 'Progress:', suffix = 'Complete', length = 50)
