d47c4b9c3a6eb477f72c23196d6b9dc3dec31530
Ryan Twohey <rjtwoh01@gmail.com>
2022-04-18 19:47:37 -0400

    now tracks avg infections per day

--- exam2/virus.py
+++ exam2/virus.py
@@ -1,6 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from progress import printProgressBar
 
 class Employee:
     def __init__(self):
@@ -16,9 +17,11 @@ def simulation():
     employeeCount = int(input('enter the number of employees: '))
     simulationCount = int(input('enter the number of times to run the simulation: '))
     dayCount = int(input('enter the number of days each simulation should run: '))
-    counter = 0
+    innerCounter = 0
     infectedComputers = []
-
+    dayCounter = dict()
+    progressTotal = simulationCount * employeeCount * dayCount
+    printProgressBar(0, progressTotal, prefix = 'Progress:', suffix = 'Complete', length = 50)
     for counter in range(0,simulationCount):
         employeeList = list()
         for i in range(0, employeeCount):
@@ -28,15 +31,17 @@ def simulation():
         employeeIndex = random.randint(0, len(employeeList) - 1)
         employeeList[employeeIndex].computer.infectedCount = 1
         for day in range(0,dayCount):
+            todaysInfected = 0
             for employee in employeeList:
                 #open existingMessages
-                print(employee.messages)
+                # print(employee.messages)
                 for message in employee.messages:
                     #you can only get infected once per day
                     infectedThisTime = False
                     infectionTransmission = random.randint(0, 100)
                     if message == 'virus' and not infectedThisTime and infectionTransmission < 40:
                         employee.computer.infectedCount += 1
+                        todaysInfected += 1
                         infectedThisTime = True
                         if employee.computer not in infectedComputers:
                             infectedComputers.append(employee.computer)
@@ -51,7 +56,28 @@ def simulation():
                     if (employeeIndex not in messagesSent) & (employeeIndex != employeeList.index(employee)):
                         messagesSent.append(employeeIndex)
                         employeeList[employeeIndex].messages.append(messageContents)
-    
-    print('total number of computers infected:', len(infectedComputers))
 
-simulation()
+                innerCounter += 1
+                printProgressBar(innerCounter, progressTotal, prefix = 'Progress:', suffix = 'Complete', length = 50)
+            if day in dayCounter:
+                dayCounter[day] += todaysInfected
+            else:
+                dayCounter[day] = todaysInfected
+    
+    # print('total number of computers infected:', len(infectedComputers))
+    return (infectedComputers, dayCounter, simulationCount)
+
+infectedComputers, dayCounter, simulationCount = simulation()
+
+for day in dayCounter:
+    dayCounter[day] = dayCounter[day] / simulationCount
+
+days = dayCounter.keys()
+infections = dayCounter.values()
+
+plt.plot(days, infections)
+plt.title('Average infections per day')
+plt.xlabel('Day')
+plt.ylabel('Infections')
+
+plt.show()
